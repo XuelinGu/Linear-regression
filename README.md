868f70df194020b7efcfa8f89daf18fb7dd65d52
Xuelin Gu <xuelingu@umich.edu>
2019-11-24 00:40:59 -0500

    add test samples

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Linear-regression
+# Linearregression
 <!-- badges: start -->
   [![Codecov test coverage](https://codecov.io/gh/XuelinGu/Linear-regression/branch/master/graph/badge.svg)](https://codecov.io/gh/XuelinGu/Linear-regression?branch=master)
   <!-- badges: end -->
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,30 @@
 library(testthat)
 library(stats)
 library(Linearregression)
-y=c(1,2,3,4,5)
-x1=c(1,2,3,4,5)
-x2=c(3,4,2,5,2)
 
+####testing small samples
+y = c(23,24,26,37,38,25,36,40)
+x1 = c(1,2,3,4,5,6,7,8)
+x2 = c(23,32,34,20,24,56,34,24)
+x3 = c("M","F","F","U","M","F","U","M")
+x4 = mtcars$hp
 test_that("Linearregression", {
-  expect_equal(lr(y~x1+x2)[[1]][1,1],lm(y~x1+x2)[[1]][[1]])
+  ##compare the estimated coeffecients
+  expect_equal(as.numeric(lr(y~x1+x2)[[1]][,1]),as.numeric(lm(y~x1+x2)[[1]]))
+  expect_equal(as.numeric(lr(y~x1+x2+x3,reference='F')[[1]][,1]),as.numeric(lm(y~x1+x2+x3)[[1]]))
+  expect_equal(as.numeric(lr(mtcars$mpg~mtcars$cyl+mtcars$disp)[[1]][,1]),as.numeric(lm(mtcars$mpg~mtcars$cyl+mtcars$disp)[[1]]))
+  expect_equal(as.numeric(lr(mpg~cyl+disp+x4,data = mtcars)[[1]][,1]),as.numeric(lm(mpg~cyl+disp+x4,data = mtcars)[[1]]))
 })
+
+
+
+####testing big samples
+set.seed(4)
+y = rnorm(1e+3,1,10)
+x1 = rnorm(1e+3,1,10)
+x2 = rnorm(1e+3,1,10)
+test_that("Linearregression", {
+  ##compare the estimated coeffecients
+  expect_equal(as.numeric(lr(y~x1+x2)[[1]][,1]),as.numeric(lm(y~x1+x2)[[1]]))
+  })
+
